ceb751918edefee06a79f2907049fde6bb78cae5
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-01-20 01:39:55 -0300

    fix build warnings

--- src/components/benefits/benefits.jsx
+++ src/components/benefits/benefits.jsx
@@ -7,19 +7,19 @@ import Link from "../link/link";
 import './benefits.css'
 
 export default function Benefits() {
-    const qualityText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, \
-    quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed \
-    nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in.";
+    const qualityText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, " +
+    "quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed " +
+    "nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in.";
 
-    const activitiesText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, \
-    quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed \
-    nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in. Curabitur consequat \
-    massa vitae faucibus ullamcorper.";
+    const activitiesText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, " +
+    "quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed " +
+    "nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in. Curabitur consequat " +
+    "massa vitae faucibus ullamcorper.";
 
-    const relaxationText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, \
-    quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed \
-    nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in. Curabitur consequat \
-    massa vitae faucibus ullamcorper. Curabitur ante magna, dictum quis aliquam eu, varius vitae sapien.";
+    const relaxationText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras blandit placerat odio, " +
+    "quis pretium nulla venenatis sit amet. Sed scelerisque odio in nisl aliquam consequat. Mauris cursus sed " +
+    "nulla sed tempor. Aliquam fringilla nibh eros, sit amet elementum purus venenatis in. Curabitur consequat " +
+    "massa vitae faucibus ullamcorper. Curabitur ante magna, dictum quis aliquam eu, varius vitae sapien.";
 
 
     return (
--- src/hooks/useActiveVacancies.js
+++ src/hooks/useActiveVacancies.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import useFetch from "./useFetch";
 
 export default function useActiveVacancies() {
